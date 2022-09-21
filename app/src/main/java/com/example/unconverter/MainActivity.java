54029c2418bb271faa9ed27ccf2e3ef396d60bd7
Hina <hina.nida@gmail.com>
2022-09-21 19:26:56 +0500

    Unit Converter Hina First Github App

---
+++ app/src/main/java/com/example/unconverter/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.unconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button convertBtn;
+    private TextView resultText;
+    private EditText kgValue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        convertBtn = findViewById(R.id.convert);
+        resultText = findViewById(R.id.result);
+        kgValue = findViewById(R.id.kgValue);
+
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hi Click Listener Worked", Toast.LENGTH_SHORT).show();
+                 String s = kgValue.getText().toString();
+                 int kg= Integer.parseInt(s);
+                 double pound = kg*2.205;
+                 resultText.setText("The value in pounds is "+pound);
+            }
+        });
+    }
+}
